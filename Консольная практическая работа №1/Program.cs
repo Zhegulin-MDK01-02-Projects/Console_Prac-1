a248067cff3b53727c2008cbf8c148eea7f247d2
MatveyZh404 <118770204+MatveyZh404@users.noreply.github.com>
2023-05-21 22:29:50 +0300

    Add files via upload

---
+++ Консольная практическая работа №1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Решение 1
+double X = (26 / Math.Sqrt(Math.Pow(((34.06 - 33.81) * 4 / (6.84 / Math.Sqrt(Math.Pow(28.57 - 25.15, 1 / 5)))) - Math.Sqrt((3 + 4.2 / 0.1) / ((1 / 0.3 - 2.7) / 4.12)),1/7)))+(2/3/4/21)+((1+Math.Sqrt(Math.Pow(5, 1/3)))/3.5);
+Console.WriteLine(Convert.ToString(X));
+//Решение 2
+double A = 26 / Math.Sqrt(Math.Pow(((34.06 - 33.81) * 4 / (6.84 / Math.Sqrt(Math.Pow(28.57 - 25.15, 1 / 5)))) - Math.Sqrt((3 + 4.2 / 0.1) / ((1 / 0.3 - 2.7) / 4.12)), 1 / 7));
+double B = 2 / 3 / 4 / 21;
+double C = (1 + Math.Sqrt(Math.Pow(5, 1 / 3))) / 3.5;
+double Y = A + B + C;
+Console.WriteLine(Convert.ToString(Y));
